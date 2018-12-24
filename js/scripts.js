de70578a8a3da7ed6613a834e867490db14dfc67
Ted <>
2018-12-24 08:10:00 -0800

    added overlay with project details while hovering over image in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-
 $(document).ready(function() {
-  $(".project-preview").on("click",function() {
-    $(this).siblings(".project-details").slideToggle();
-  });
-};
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
